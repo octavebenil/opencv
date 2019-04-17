25625b12a3d460ade068e521718f2a681b58c700
ben <octavebenil@gmail.com>
2019-04-17 10:13:36 +0300

    Fanovana structure projet sy code vitsivitsy

--- detection_voiture.py
+++ detection_voiture.py
@@ -1,6 +1,8 @@
 """
 detection automobile amin'ny video, mampiasa opencv sy fichier xml haar cars.xml
 
+Mitovy amin'ny detection visage ihany koa fa ny fichier cascade fotsiny no miova
+
 """
 import cv2
 
@@ -12,14 +14,12 @@ video = cv2.VideoCapture('./datasets/video.avi')
 while video.isOpened():
     ret, frame = video.read()
     if ret:
-        temps = cv2.getTickCount()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
         cars = car_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=4)
+
         for x, y, w, h in cars:
             cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
-
-        fps = cv2.getTickFrequency()/(cv2.getTickCount() - temps)
-        cv2.putText(frame, "FPS: {:05.2f}".format(fps), (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
 
         cv2.imshow('Voitures', frame)
 
